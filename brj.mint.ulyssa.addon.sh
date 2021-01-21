4364b497e000b8e39e2ee5debdb8b63f272f7b03
Roman Y. Bogdanov <sam@brj.pp.ru>
2021-01-21 21:46:35 +0300

    2021-01-21-214635 This bunny should be killed.

---
+++ brj.mint.ulyssa.addon.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+export DEBIAN_FRONTEND=noninteractive
+
+mkdir -p /tmp/debs
+cd /tmp/debs
+
+wget -nc https://www.rescuetime.com/installers/rescuetime_current_amd64.deb
+wget -nc https://mega.nz/linux/MEGAsync/xUbuntu_20.04/amd64/nemo-megasync-xUbuntu_20.04_amd64.deb
+wget -nc https://mega.nz/linux/MEGAsync/xUbuntu_20.04/amd64/megasync-xUbuntu_20.04_amd64.deb
+
+mkdir -p ~/.local/share/fonts/
+
+wget -nc https://github.com/be5invis/Iosevka/releases/download/v4.5.0/ttf-iosevka-4.5.0.zip
+
+echo "copy font to ~/.local/share/fonts/ and run fc-cache -fv"
+
