fa1e3032ca4d6ecd74bb29b9ab8604108a968f37
Roman Y. Bogdanov <sam@brj.pp.ru>
2021-01-21 21:45:12 +0300

    2021-01-21-214511 This should work until december 2013.

--- commit.sh
+++ commit.sh
@@ -13,4 +13,4 @@ echo run git push on ${dated}
 for i in `git status | grep deleted | awk '{print $2}'`; do git rm $i; done
 
 git commit -m "$dated $(curl -s http://whatthecommit.com/index.txt)"
-git push -u origin master
+git push -u origin main
