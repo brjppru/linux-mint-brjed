59ef9eaabd77266f0f36696c919164eb999ab827
Roman Y. Bogdanov <sam@brj.pp.ru>
2021-01-23 07:13:37 +0300

    2021-01-23-071337 I am Root. We are Root.

--- home/brj.bgimage/makeme.sh
+++ home/brj.bgimage/makeme.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # imagemagick tile same image
+# get display by xrandr | grep "*"
 
 #convert 090127-2121-45.bmp -write mpr:tile  +delete -size 1366x768 tile:mpr:tile brj_wallpaper.jpg
 convert 090127-2121-45.bmp -write mpr:tile  +delete -size 1920x1080 tile:mpr:tile brj_wallpaper.jpg
