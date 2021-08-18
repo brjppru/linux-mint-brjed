ca41decad992be572edc953ed6c337a0e3f4c864
root <root@15ABR>
2021-08-18 10:41:08 +0300

    2021-08-18-104107 the magic is real

--- addon/mc.mint.addon.sh
+++ addon/mc.mint.addon.sh
@@ -12,7 +12,6 @@ curl -fsSL http://www.tataranovich.com/debian/gpg | sudo apt-key add -
 APT_SOURCE='deb http://www.tataranovich.com/ubuntu focal main'
 echo $APT_SOURCE | sudo tee /etc/apt/sources.list.d/tataranovich.list
 
-deb http://www.tataranovich.com/ubuntu focal main
-deb-src http://www.tataranovich.com/ubuntu focal main
-
+deb [arch=amd64]  http://www.tataranovich.com/ubuntu focal main
+deb-src [arch=amd64]  http://www.tataranovich.com/ubuntu focal main
 
