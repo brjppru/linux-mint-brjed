a604db30cbc1743e4c69a3d48d1ae38c9f34ac3d
Дымито Паравоскес <paravoskes@yahoo.com>
2021-08-19 17:57:51 +0300

    2021-08-19-175751 Another bug bites the dust

--- addon/thunder.addon.sh
+++ addon/thunder.addon.sh
@@ -1,9 +1,19 @@
 #!/bin/bash -x
 
+# outdated
+
+# use Hide Local Folders for TB78++
+# Move Mail Shortcuts
+# https://github.com/robert7k/moveMailShortcuts
+
+exit 0
+
 # https://launchpad.net/ubuntu/focal/amd64/thunderbird/1:68.10.0+build1-0ubuntu0.20.04.1
 # https://launchpad.net/ubuntu/focal/amd64/thunderbird-gnome-support/1:68.10.0+build1-0ubuntu0.20.04.1
 
 export DEBIAN_FRONTEND=noninteractive
+
+exit 0
 
 sudo apt-mark unhold thunderbird thunderbird-gnome-support
 sudo apt purge -y thunderbird thunderbird-gnome-support
