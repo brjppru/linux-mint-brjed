b85ebba38e5da29466508289de4fce3b1a6cb1aa
Дымито Паравоскес <paravoskes@yahoo.com>
2021-08-24 17:58:18 +0300

    2021-08-24-175818 Misc. fixes

--- docker/uma.webdav.sh
+++ docker/uma.webdav.sh
@@ -9,8 +9,10 @@ echo y | docker system prune --volumes
 sudo mkdir -p /var/spool/dav
 
 docker run --restart always -v /var/spool/dav:/var/lib/dav \
-    -e AUTH_TYPE=Digest -e USERNAME=zalupa -e PASSWORD=zalupa1234 \
-    --publish 6677:80 --name webdav \
+    -e AUTH_TYPE=Digest \
+    -e USERNAME=zalupa \
+    -e PASSWORD=koshachiya \
+    --publish 6677:80 \
+    --name webdav \
     -e LOCATION=/webdav \
     -d bytemark/webdav
-
