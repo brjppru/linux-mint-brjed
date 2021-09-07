b96dab7af4d633d1fe343f2a421c903ab6786d34
Grzegorz Brzęczyszczykiewicz <paravoskes@yahoo.com>
2021-09-07 14:47:11 +0300

    2021-09-07-144710 This should work until december 2013.

--- commit.sh
+++ commit.sh
@@ -13,4 +13,6 @@ for i in `git status | grep deleted | awk '{print $2}'`; do git rm $i; done
 git commit -m "$dated $(curl -s http://whatthecommit.com/index.txt)"
 git push -u origin main
 
+#
 # non master, just a main. fck2020 (c) scooter
+#
