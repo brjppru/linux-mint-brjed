f4c827e012a8db60605f6ca0f7ff4154e6384bb0
Grzegorz Brzęczyszczykiewicz <sam@brj.pp.ru>
2021-09-18 13:54:37 +0300

    2021-09-18-135436 Fix hard-coded [object Object] string (thanks!)

---
+++ addon/brj.mint.exa.down.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+
+export DEBIAN_FRONTEND=noninteractive
+
+# need run? fix me
+
+exit
+
+rm -rf /tmp/exa
+mkdir -p /tmp/exa
+cd /tmp/exa
+
+wget https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip
+
+echo "pochti. all done"
+
+unzip -o exa-linux-x86_64-v0.10.1.zip
+
+sudo cp /tmp/exa/bin/exa /usr/local/bin
+sudo cp /tmp/exa/completions/exa.bash /etc/bash_completion.d
+sudo cp /tmp/exa/man/exa_colors.5 /usr/share/man/man1
+sudo cp /tmp/exa/man/exa.1 /usr/share/man/man1
+
+/usr/local/bin/exa --long /usr/local/bin/exa /etc/bash_completion.d/exa.bash /usr/share/man/man1/exa.1
+
+sudo apt -y --fix-broken install
+
