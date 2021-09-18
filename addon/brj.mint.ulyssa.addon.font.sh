fca48d7a2656286949330dbfe5c6f1d2322ff8e1
Grzegorz Brzęczyszczykiewicz <sam@brj.pp.ru>
2021-09-18 12:40:58 +0300

    2021-09-18-124057 Derp search/replace fuckup

--- addon/brj.mint.ulyssa.addon.font.sh
+++ addon/brj.mint.ulyssa.addon.font.sh
@@ -4,9 +4,9 @@ export DEBIAN_FRONTEND=noninteractive
 
 # need run? fix me
 
-# https://github.com/ryanoasis/nerd-fonts/releases
+exit
 
-exit 0
+# https://github.com/ryanoasis/nerd-fonts/releases
 
 mkdir -p ~/.local/share/fonts/
 cd ~/.local/share/fonts/
@@ -20,6 +20,7 @@ https://github.com/be5invis/Iosevka/releases/download/v4.5.0/ttf-iosevka-4.5.0.z
 https://github.com/be5invis/Iosevka/releases/download/v4.5.0/ttf-iosevka-fixed-4.5.0.zip
 https://github.com/microsoft/cascadia-code/releases/download/v2106.17/CascadiaCode-2106.17.zip
 https://github.com/tonsky/FiraCode/releases/download/5.2/Fira_Code_v5.2.zip
+https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
 https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/IBMPlexMono.zip
 EOF
 
