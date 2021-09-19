6b448dd7b37adf10ba917f9e87ee0771a8242a86
Grzegorz Brzęczyszczykiewicz <sam@brj.pp.ru>
2021-09-19 13:48:10 +0300

    2021-09-19-134810 omg what have I done?

--- hware/fuck.ipv6.sh
+++ hware/fuck.ipv6.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+# fuck ipv6
+
 sudo tee <<EOF /etc/sysctl.d/no.ipv6.conf >/dev/null
 net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv6.conf.default.disable_ipv6 = 1
 net.ipv6.conf.lo.disable_ipv6 = 1
 EOF
 
+# fuck gai
+#precedence ::ffff:0:0/96  100
+
+sudo sed -i 's/#precedence ::ffff:0:0\/96  100/precedence ::ffff:0:0\/96  100/' /etc/gai.conf
