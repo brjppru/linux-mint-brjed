5dec6c218eecb5fe2b03f8c6d3ce04b38347a3eb
Grzegorz Brzęczyszczykiewicz <sam@brj.pp.ru>
2021-09-25 01:12:13 +0300

    2021-09-25-011213 refuckulated the carbonator

--- docker/gixy/gixy.sh
+++ docker/gixy/gixy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# see https://www.digitalocean.com/community/tools/nginx
+
 exit 0
 
 docker run --rm -v `pwd`/nginx.conf:/etc/nginx/conf/nginx.conf yandex/gixy /etc/nginx/conf/nginx.conf
