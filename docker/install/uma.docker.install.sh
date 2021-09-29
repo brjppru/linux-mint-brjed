72cfa5f56fc218570d526d6d279f808352e8239d
Grzegorz Brzęczyszczykiewicz <paravoskes@yahoo.com>
2021-09-29 09:27:32 +0300

    2021-09-29-092731 Pipeline goes brrrrrrr

--- docker/install/uma.docker.install.sh
+++ docker/install/uma.docker.install.sh
@@ -38,6 +38,7 @@ sudo apt update
 
 sudo apt -y install docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
